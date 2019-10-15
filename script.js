cd325c01cfd171ea7a2c2278e007045d639e6966
NathanNeelis <55492381+NathanNeelis@users.noreply.github.com>
2019-10-15 22:00:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,44 @@ var naamDoosje;
 naamDoosje = "Nathan";
 console.log(naamDoosje);
 
+
+
+//TOGGLE PRIJS INDICATOR ----------------------------------------------
 var clickButton = document.getElementById('prijs');
 var veld = document.querySelectorAll('form');
 console.log(veld[2]);
 
 function show() {
     console.log("check");
-    veld[2].classList.toggle('subFormShow');
+    veld[2].classList.toggle('formPrijsShow');
 }
 
 clickButton.addEventListener('click', show);
+
+
+//SLIDER INDICATOR ----------------------------------------------
+var slider = document.getElementById("prijsIndicatie");
+var output = document.getElementById("bedrag");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+};
+
+
+//TOGGLE DATE INDICATOR ----------------------------------------------
+
+var clickButtonDatum = document.getElementById('datum');
+var veldDatum = document.querySelectorAll('form');
+console.log(veld[3]);
+
+function showDatum() {
+    console.log("check");
+    veldDatum[3].classList.toggle('formDateShow');
+}
+
+clickButtonDatum.addEventListener('click', showDatum);
+
+
+
+
